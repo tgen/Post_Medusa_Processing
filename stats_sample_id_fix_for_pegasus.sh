848604801d9ebe2d6eedf2b612ba5e7aeb41db59
awchrist <achristofferson@tgen.org>
2017-02-07 12:38:39 -0700

    Added script to change summary all stats file to work with KBase

---
+++ stats_sample_id_fix_for_pegasus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+for line in `ls -d */ |cut -d/ -f1`
+do 
+	cd $line
+	
+	if [ ! -f rename_stats_complete.txt ]
+	then
+	
+	for row in `grep "FQ=" ${line}.config | awk -F'/' '{print $8 }' | awk -F'_' '{ OFS = "_" ; print $2,$3,$4,$5,$6,$7,$8,$9 }' | sort | uniq`
+	do 
+		SAMPLE_NAME=`echo $row | awk -F'_' '{ OFS = "_" ; print $1,$2,$3,$4,$5,$6,$7 }'` 
+		SAMPLE_ID=`echo $row | awk -F'_' '{ OFS = "_" ; print $8 }'`
+		ASSAY=`echo $row | awk -F'_' '{ OFS = "_" ; print $7 }'`
+
+		sed "s/${SAMPLE_NAME}	${ASSAY}/${row}	${SAMPLE_ID}/g" stats/Summary_allStats.txt > stats/Summary_allStats1.txt
+		
+		mv stats/Summary_allStats1.txt stats/Summary_allStats.txt		
+		for file in `ls stats/${SAMPLE_NAME}*`
+		do 
+			ENDING=`echo $file | cut -d. -f2-`
+			mv ${file} stats/${row}.${ENDING}
+		done
+	done	
+	
+	touch rename_stats_complete.txt
+	
+	fi
+	
+	cd ..
+done
